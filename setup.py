f202e6977ecdd3e2afdae4549aa70f1ca7adfb8c
MAT34525 <123415140+MAT34525@users.noreply.github.com>
2025-01-14 14:07:44 +0100

    Initial setup.py file and Readme.md update

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+ name='Todo-Decker-CDOF3',
+ version='1.0',
+ author='Mathys DECKER}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
